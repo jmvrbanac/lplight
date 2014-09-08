83e75cb6b0c51ba5bd9636614bac4f4a2c8616a1
John Vrbanac <john.vrbanac@linux.com>
2014-09-08 01:46:32 -0500

    Nesting model specs

--- spec/models.py
+++ spec/models.py
@@ -10,67 +10,66 @@ class SampleModelImpl(models.SimpleModel):
         self.field_b = None
 
 
-class SimpleModel(Spec):
+class ModelSpecs(Spec):
 
-    def can_deserialize(self):
-        result = SampleModelImpl.from_json({
-            'field_a': 'Tester',
-            'field_b': True
-        })
+    class Base(Spec):
 
-        expect(result).not_to.be_none()
-        expect(result.field_a).to.equal('Tester')
-        expect(result.field_b).to.be_true()
+        def can_deserialize(self):
+            result = SampleModelImpl.from_json({
+                'field_a': 'Tester',
+                'field_b': True
+            })
 
+            expect(result).not_to.be_none()
+            expect(result.field_a).to.equal('Tester')
+            expect(result.field_b).to.be_true()
 
-class ProjectModel(Spec):
+    class Project(Spec):
 
-    def can_deserialize(self):
-        name = 'Test Name'
-        title = 'Test Title'
-        bug_tracker_link = 'Test link'
+        def can_deserialize(self):
+            name = 'Test Name'
+            title = 'Test Title'
+            bug_tracker_link = 'Test link'
 
-        sample_dict = {
-            'name': name,
-            'title': title,
-            'bug_tracker_link': bug_tracker_link
-        }
+            sample_dict = {
+                'name': name,
+                'title': title,
+                'bug_tracker_link': bug_tracker_link
+            }
 
-        project = models.Project.from_json(sample_dict)
+            project = models.Project.from_json(sample_dict)
 
-        expect(project.name).to.equal(name)
-        expect(project.title).to.equal(title)
-        expect(project.bug_tracker_link).to.equal(bug_tracker_link)
+            expect(project.name).to.equal(name)
+            expect(project.title).to.equal(title)
+            expect(project.bug_tracker_link).to.equal(bug_tracker_link)
 
+    class Bug(Spec):
 
-class BugModel(Spec):
+        def can_deserialize(self):
+            sample_dict = {
+                'name': 'Test Name',
+                'title': 'Test Title',
+                'id': 'Test id',
+                'bug_tasks_collection_link': 'bug task link'
+            }
 
-    def can_deserialize(self):
-        sample_dict = {
-            'name': 'Test Name',
-            'title': 'Test Title',
-            'id': 'Test id',
-            'bug_tasks_collection_link': 'bug task link'
-        }
+            bug = models.Bug.from_json(sample_dict)
 
-        bug = models.Bug.from_json(sample_dict)
+            for key, val in sample_dict.items():
+                bug_attr = getattr(bug, key)
+                expect(bug_attr).to.equal(sample_dict[key])
 
-        for key, val in sample_dict.items():
-            bug_attr = getattr(bug, key)
-            expect(bug_attr).to.equal(sample_dict[key])
+    class BugTask(Spec):
 
+        def can_deserialize(self):
+            sample_dict = {
+                'date_assigned': 'Test assigned',
+                'title': 'Test Title',
+                'date_closed': 'stuff'
+            }
 
-class BugTaskModel(Spec):
+            bug = models.BugTask.from_json(sample_dict)
 
-    def can_deserialize(self):
-        sample_dict = {
-            'date_assigned': 'Test assigned',
-            'title': 'Test Title',
-            'date_closed': 'stuff'
-        }
-
-        bug = models.BugTask.from_json(sample_dict)
-
-        for key, val in sample_dict.items():
-            bug_attr = getattr(bug, key)
-            expect(bug_attr).to.equal(sample_dict[key])
+            for key, val in sample_dict.items():
+                bug_attr = getattr(bug, key)
+                expect(bug_attr).to.equal(sample_dict[key])
